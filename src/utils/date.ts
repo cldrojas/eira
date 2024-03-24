0cbd86a5b63be380a7b006c5f6aa4e173689f54d
cldrojas <cld.rojasz@gmail.com>
2024-03-24 01:48:16 -0300

    lint and minor fixes

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,19 +1,22 @@
-export const toLocal = (date: Date | string) => {
-  const tz = Intl.DateTimeFormat().resolvedOptions().timeZone
-  const d = new Date(date);
-  const res = format(d.getTime(), tz);
-  return res;
-};
+export const toLocal = (date: Date | string, opts?: Intl.DateTimeFormatOptions ) => {
+	const tz = Intl.DateTimeFormat().resolvedOptions().timeZone
+	const d = new Date(date)
+	const res = format(d.getTime(), tz, opts)
+	return res
+}
 
-const format = (timestamp: number, timeZone: string) => {
-  const options: Intl.DateTimeFormatOptions = {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "numeric",
-    timeZone,
-    hour12: false,
-  };
-  return new Intl.DateTimeFormat("es-ES", options).format(new Date(timestamp));
-};
+const format = (
+	timestamp: number,
+	timeZone: string,
+	options: Intl.DateTimeFormatOptions = {
+		day: 'numeric',
+		month: 'long',
+		year: 'numeric',
+		hour: '2-digit',
+		minute: 'numeric',
+		timeZone,
+		hour12: false,
+	}
+) => {
+	return new Intl.DateTimeFormat('es-ES', options).format(new Date(timestamp))
+}
