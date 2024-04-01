d0856861129ffa9924f9f24c4a556627674161dd
cldrojas <cld.rojasz@gmail.com>
2024-04-01 11:32:09 -0300

    add todo interface

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,22 +1,33 @@
+/// <reference path="../.astro/db-types.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
 interface Note {
 	id: string
 	title: string
-	description?: string
 	content: string
-	todos?: string[]
+	description?: string
 	reminders?: string
 	attachments?: string[]
 	createdAt: Date
 	updatedAt: Date
 }
+
+interface Todo {
+	id: string
+	title: string
+	completed: boolean
+	noteId: string
+}
+
 interface Appointment {
 	id: string
 	title: string
 	date: Date
-	place?: string
+	place?: {
+		name: string
+		location: string
+	}
 }
 interface topBartopBarActions {
 	className: string
