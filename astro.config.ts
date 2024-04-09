f54d5c1149aa82c741df48d4b00f3472ec32c80d
cldrojas <cld.rojasz@gmail.com>
2024-04-08 22:52:53 -0400

    dployed to vercell

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // import cloudflare from '@astrojs/cloudflare'
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-	// compressHTML: true,
-	// prefetch: true,
-	devToolbar: {
-		enabled: false,
-	},
-	integrations: [tailwind()],
-	output: 'server',
-	// adapter: cloudflare(),
-})
+  // compressHTML: true,
+  // prefetch: true,
+  devToolbar: {
+    enabled: false
+  },
+  integrations: [tailwind()],
+  output: 'server'
+  // adapter: cloudflare(),
+  ,
+  adapter: vercel()
+});
