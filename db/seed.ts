81165e8bb26cb85281333ac28c433d23edfa0fc0
cldrojas <cld.rojasz@gmail.com>
2024-04-14 18:57:27 -0400

    enable db configs and seed data

--- db/seed.ts
+++ db/seed.ts
@@ -1,37 +1,35 @@
-// import { Notes, db } from 'astro:db'
+import { db, Notes } from "astro:db";
 
 const NOTES = [
-	{
-		id: 'first-note',
-		title: 'First Note',
-		description: 'Description 1',
-		content: 'Content 1',
-		createdAt: new Date('2021-01-01'),
-		updatedAt: new Date(),
-	},
-	{
-		id: 'second-note',
-		title: 'Second Note',
-		description: 'Description 2',
-		content: 'Content 2',
-		createdAt: new Date('2024-02-04'),
-		updatedAt: new Date(),
-	},
-]
+  {
+    id: "first-note",
+    title: "First Note",
+    description: "Description 1",
+    content: "Content 1",
+    createdAt: new Date("2021-01-01"),
+    updatedAt: new Date(),
+  },
+  {
+    id: "second-note",
+    title: "Second Note",
+    description: "Description 2",
+    content: "Content 2",
+    createdAt: new Date("2024-02-04"),
+    updatedAt: new Date(),
+  },
+];
 
 // https://astro.build/db/seed
 export default async function seed() {
-	const notes = NOTES.map(
-		({ id, title, description, content, createdAt, updatedAt }) => ({
-			id,
-			title,
-			description: description || '',
-			content,
-			createdAt,
-			updatedAt,
-		})
-	)
-	// await db.insert(Notes).values(notes)
-	// const teams = NOTES.flatMap(({ boxers, teams }) => teams ?? boxers).map((id) => ({ id }))
-	// await db.insert(VoteSelections).values(teams)
+  const notes = NOTES.map(
+    ({ id, title, description, content, createdAt, updatedAt }) => ({
+      id,
+      title,
+      description: description || "",
+      content,
+      createdAt,
+      updatedAt,
+    })
+  );
+  await db.insert(Notes).values(notes);
 }
