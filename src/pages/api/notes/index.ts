8ac2f3f4203d5781cdb740e2468c6b2b42d17fd1
cldrojas <cld.rojasz@gmail.com>
2024-04-14 19:12:40 -0400

    enable add note feature

--- src/pages/api/notes/index.ts
+++ src/pages/api/notes/index.ts
@@ -1,20 +1,21 @@
-import { toSlug } from '@/utils/string'
-import type { APIRoute } from 'astro'
-// import { db, Notes, NOW } from 'astro:db'
+import { toSlug } from "@/utils/string";
+import type { APIRoute } from "astro";
+import { db, Notes, NOW } from "astro:db";
 
 export const POST: APIRoute = async ({ params, request }) => {
-	// const {_note} = params
-	console.log(`DEBUG:params:`, params)
-	const note = await request.json()
-	note.id = toSlug(note.title)
-	// note.createdAt = NOW
-	// note.updatedAt = NOW
+  const { _note } = params;
+  console.log(`DEBUG:params:`, params);
+  const note = await request.json();
+  note.id = toSlug(note.title);
+  note.createdAt = NOW;
+  note.updatedAt = NOW;
 
-	try {
-		// await db.insert(Notes).values(note)
-	} catch (error) {
-		console.error(error)
-		return new Response('Error adding note!', { status: 500 })
-	}
-	return new Response('Note added!', { status: 200 })
-}
+  try {
+    console.log(`DEBUG:trying to add note:`, note);
+    await db.insert(Notes).values(note);
+  } catch (error) {
+    console.error(error);
+    return new Response("Error adding note!", { status: 500 });
+  }
+  return new Response("Note added!", { status: 200 });
+};
