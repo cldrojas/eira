9a037a77f6d6b1613ca98d4ace5627fb9e4d7f9b
cldrojas <cld.rojasz@gmail.com>
2024-04-14 21:11:00 -0400

    fix whings

--- src/pages/api/notes/index.ts
+++ src/pages/api/notes/index.ts
@@ -1,6 +1,7 @@
-import { toSlug } from "@/utils/string";
-import type { APIRoute } from "astro";
 import { db, Notes, NOW } from "astro:db";
+import type { APIRoute } from "astro";
+
+import { toSlug } from "@/utils/string";
 
 export const POST: APIRoute = async ({ params, request }) => {
   const { _note } = params;
@@ -17,5 +18,23 @@ export const POST: APIRoute = async ({ params, request }) => {
     console.error(error);
     return new Response("Error adding note!", { status: 500 });
   }
-  return new Response("Note added!", { status: 200 });
+
+  return new Response(`Note ${note.id} added!`, { status: 200 });
+};
+
+export const PUT: APIRoute = async ({ params, request }) => {
+  const { _note } = params;
+  console.log(`DEBUG:params:`, params);
+  const note = await request.json();
+  note.updatedAt = NOW;
+
+  try {
+    console.log(`DEBUG:trying to update note:`, note);
+    // await db.update(Notes).set(note).where({ id: _note });
+  } catch (error) {
+    console.error(error);
+    return new Response("Error updating note!", { status: 500 });
+  }
+
+  return new Response("Note updated!", { status: 200 });
 };
