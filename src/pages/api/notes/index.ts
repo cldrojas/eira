1841c5753292accae8a871946954fbd551b30100
cldrojas <cld.rojasz@gmail.com>
2024-04-19 12:07:21 -0400

    add speed insights

--- src/pages/api/notes/index.ts
+++ src/pages/api/notes/index.ts
@@ -1,11 +1,17 @@
 import { db, Notes, NOW } from "astro:db";
 import type { APIRoute } from "astro";
+import { getSession } from "auth-astro/server";
 
 import { toSlug } from "@/utils/string";
 
 export const POST: APIRoute = async ({ params, request }) => {
   const { noteId } = params;
-
+  const session = await getSession(request);
+  if (!session) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+  const user = session.user;
+  console.log(`DEBUG:user:`, user)
   const note = await request.json();
   note.id = noteId ?? toSlug(note.title);
   note.createdAt = note.createdAt || NOW;
