55b508271ed238e90d4a4ece71e59ed0803315b3
cldrojas <cld.rojasz@gmail.com>
2024-04-19 00:46:14 -0400

    add upsert

--- src/data/notes.ts
+++ src/data/notes.ts
@@ -1,1 +1,8 @@
-// Migrated to astro:db
+// lib notes direct call to the database
+import { db, Notes, NOW } from "astro:db";
+
+const getNotes = () => {};
+
+export const saveNote = async (data: { [k: string]: FormDataEntryValue }) => {
+  console.log(`DEBUG:data to send for save:`, data);
+};
--- src/pages/api/notes/index.ts
+++ src/pages/api/notes/index.ts
@@ -4,37 +4,26 @@ import type { APIRoute } from "astro";
 import { toSlug } from "@/utils/string";
 
 export const POST: APIRoute = async ({ params, request }) => {
-  const { _note } = params;
-  console.log(`DEBUG:params:`, params);
+  const { noteId } = params;
+
   const note = await request.json();
-  note.id = toSlug(note.title);
-  note.createdAt = NOW;
+  note.id = noteId ?? toSlug(note.title);
+  note.createdAt = note.createdAt || NOW;
   note.updatedAt = NOW;
 
   try {
     console.log(`DEBUG:trying to add note:`, note);
-    await db.insert(Notes).values(note);
+    await db
+      .insert(Notes)
+      .values(note)
+      .onConflictDoUpdate({ target: Notes.id, set: note });
   } catch (error) {
     console.error(error);
     return new Response("Error adding note!", { status: 500 });
   }
 
-  return new Response(`Note ${note.id} added!`, { status: 200 });
+  return new Response(`Note ${noteId ? "Updated" : "Created"}!: ${note}`, {
+    status: 200,
+  });
 };
 
-export const PUT: APIRoute = async ({ params, request }) => {
-  const { _note } = params;
-  console.log(`DEBUG:params:`, params);
-  const note = await request.json();
-  note.updatedAt = NOW;
-
-  try {
-    console.log(`DEBUG:trying to update note:`, note);
-    // await db.update(Notes).set(note).where({ id: _note });
-  } catch (error) {
-    console.error(error);
-    return new Response("Error updating note!", { status: 500 });
-  }
-
-  return new Response("Note updated!", { status: 200 });
-};
